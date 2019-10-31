d0a1e090082b47574292585c5a04deb43f0d900e
SaiAshishK <saiashishk@msitprogram.net>
2019-10-31 21:22:48 +0530

    .

--- module-1/sortedArray.java
+++ module-1/sortedArray.java
@@ -3,23 +3,38 @@ public class sortedArray {
     public static void main(String[] args) {
         int[] arr1 = {1,2,3,4,5,6};
         int[] arr2 = {2,4,6,8,9};
+        
+        int[] c = new int[arr1.length + arr2.length];
         int i = 0;
         int j = 0;
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
+        int key = 0;
+        //System.out.println();
+        
         while ( i <  arr1.length && j < arr2.length) {
-            if (arr1[i] == arr2[j]) {
-                System.out.println(arr1[i]);
+            if (arr1[i] <= arr2[j]){
+                c[key] = arr1[i];
+                key++;
                 i++;
+            } 
+            else {
+                c[key] = arr2[j];
                 j++;
+                key++;
+                
             }
-            else if (arr1[i] < arr2[j]) {
-                i++;
-            }       
-        else {
+                   } 
+        while (i < arr1.length){
+            c[key] = arr1[i];
+            i++;
+            key++;
+        }
+        while(j < arr2.length) {
+            c[key] = arr2[j];
+            key++;
             j++;
-        } 
+        }
+        System.out.println(Arrays.toString(c));
+        //return Arrays.toString(c);
     }
 
     }
-}
--- module-2/Parentheses Matching/Solution.java
+++ module-2/Parentheses Matching/Solution.java
@@ -1,6 +1,65 @@
+import java.util.*;
 class Solution{
 	public static String isMatching(String str){
 		// fill you code Here
-		return "YES";
+		char[] data = str.toCharArray();
+		int length = data.length;
+		Stack<Character> stack = new Stack<Character>();
+		for(int i = 0; i < length; i++)
+		{
+			if(data[i] == '(' || data[i] == '[' || data[i] == '{')
+			{
+				stack.push(data[i]);
+			}
+			else if(data[i] == ']')
+			{
+				if(stack.empty() == false)
+				{
+					if(stack.pop() != '[')
+					{
+						return "NO";
+					}
+				}
+				else{
+					return "NO";
+				}
+			}
+			else if(data[i] == '}')
+			{
+				if(stack.empty() == false)
+				{
+					if(stack.pop() != '{')
+					{
+						return "NO";
+					}
+				}
+				else
+				{
+					return "NO";
+				}
+			}
+			else if(data[i] == ')')
+			{
+				if(stack.empty() == false)
+				{
+					if(stack.pop()!='(')
+					{
+						return "NO";
+
+					}
+				}
+				else{
+					return "NO";
+				}
+			}
+		}
+		if(stack.empty() == true)
+		{
+			return "YES";
+		}
+		else{
+			return "NO";
+		}
+	
 	}
 }
