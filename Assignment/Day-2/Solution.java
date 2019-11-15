e7e0d2235346894d3a9db90fb4237a04ce950e07
SaiAshishK <saiashishk@msitprogram.net>
2019-11-15 16:19:09 +0530

    day-2 Done

---
+++ Assignment/Day-2/Solution.java
@@ -0,0 +1,80 @@
+import java.util.*;
+public class Solution
+{  
+/**
+ * @author - K. Sai Ashish
+  */ 
+    Node last;
+    int size;
+
+    public Solution() {
+        last = null;
+        size = 0;
+    }
+
+    class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+
+        }
+    }
+    /**
+     *  Time Complexity - O(1)
+     * @param value
+     * inserts a node in last
+     */
+    public void Enqueue(int value) {
+        Node temp = new Node(value);
+        if (isEmpty()) {
+            last = temp;
+            last.next = last;
+        } else {
+            temp.next = last.next;
+            last.next = temp;
+            last = temp;
+        }
+        size++;
+
+    }
+    /**
+     *  Time Complexity - O(1)
+     * deltes the first inserted node
+     */
+    public int Dequeue() {
+        if (isEmpty()) {
+            throw new NullPointerException();
+        } else {
+            Node temp = last.next;
+            last.next = last.next.next;
+            size--;
+            return temp.data;
+
+        }
+
+    }
+    // returns int size
+    public int size() {
+        return size;
+    }
+    // return boolean to know if
+    // the linked list is empty or not
+    public boolean isEmpty() {
+        return (size == 0);
+    }
+
+    public static void main(String[] args) {
+        Solution cir = new Solution();
+        cir.Enqueue(5);
+        // cir.Enqueue(54);
+        // cir.Enqueue(40);
+        // cir.Enqueue(25);
+        System.out.println(cir.Dequeue());
+        System.out.println(cir.size());
+
+        
+        }
+    }
--- Project-1/Deque.java
+++ Project-1/Deque.java
@@ -1,30 +1,94 @@
 public class Deque<Item> implements Iterable<Item> {
+    class Node{
+        int size;
+        Node prev;
+        Node next;
+        Item item;
 
+		public Node(Item item) {
+			prev = null;
+            next = null;
+            this.item = item;
+        }
+        
+    }
     // construct an empty deque
-    public Deque()
+    public Deque() {
+        first = null;
+        last = null;
+        size = 0;
+    }
 
     // is the deque empty?
-    public boolean isEmpty()
+    public boolean isEmpty(){
+        return ((last == null && first == null));
+        
+    }
 
     // return the number of items on the deque
-    public int size()
+    public int size(){
+        return size;
+    }
 
     // add the item to the front
-    public void addFirst(Item item)
+    public void addFirst(Item item){
+        Node newNode = new Node(item);
+        newNode.prev = first;
+        newNode.next = null;
+        if(first!=null){
+            first.prev = newNode;
+        }
+        first = newNode;
+
+
+    }
 
     // add the item to the back
-    public void addLast(Item item)
+    public void addLast(Item item){
+        
+            Node newNode = new Node(item);
+            last.next = newNode;
+            newNode.next = null;
+        
+        }
+
+    }
 
     // remove and return the item from the front
-    public Item removeFirst()
+    public Item removeFirst(){
+        if(size!=0) {
+            Node temp = first;
+            first=first.next;
+            first.prev = null;
+            return temp.item;
+        }
+        else{
+            return null;
+        }
+    }
 
     // remove and return the item from the back
-    public Item removeLast()
+    public Item removeLast(){
+        if(size!=0) {
+            Node temp = last;
+            last = last.prev;
+            last.next = null;
+            return temp.item;
+        }
+        else{
+            return null;
+        }
+
+    }
 
     // return an iterator over items in order from front to back
-    public Iterator<Item> iterator()
+    public Iterator<Item> iterator(){
+
+    }
 
     // unit testing (required)
-    public static void main(String[] args)
+    public static void main(String[] args){
+
+    }
 
 }
